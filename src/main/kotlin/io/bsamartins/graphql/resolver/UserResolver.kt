6757225e536896c2af7f4a42145c4e0489fd802d
Bernardo Martins <bernardo.martins@utmost.co>
2022-05-01 10:57:38 +0100

    Cleanup code

--- src/main/kotlin/io/bsamartins/graphql/resolver/UserResolver.kt
+++ src/main/kotlin/io/bsamartins/graphql/resolver/UserResolver.kt
@@ -7,12 +7,16 @@ import com.netflix.graphql.dgs.DgsQuery
 import io.bsamartins.graphql.model.types.AddressModel
 import io.bsamartins.graphql.model.types.EmploymentModel
 import io.bsamartins.graphql.model.types.UserModel
+import org.slf4j.LoggerFactory
 
 @DgsComponent
 class UserResolver {
 
+    private val log = LoggerFactory.getLogger(this::class.java)
+
     @DgsQuery(field = "findUsers")
     fun findUsers(): List<UserModel> {
+        log.info("Fetching users")
         return listOf(
             UserModel(
                 id = 1,
@@ -28,6 +32,7 @@ class UserResolver {
 
     @DgsData(parentType = "UserModel", field = "employment")
     fun getEmployment(dfe: DgsDataFetchingEnvironment): EmploymentModel {
+        log.info("Fetching employment details")
         val user = dfe.getSource<UserModel>()
         val email = user.name!!.lowercase().replace(" ", ".") + "@acme.com"
         return EmploymentModel(
